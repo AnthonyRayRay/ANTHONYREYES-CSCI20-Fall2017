8629db54d76ccbe8c8ec24ec23ab239cdb77b194
Anthony Reyes <areyes011@student.butte.edu>
2017-08-25 02:03:02 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,8 @@
+A computer will select a number between 1-50.
+User will guess a number between 1-50 from the computer.
+User will have 10 points (guesses) to choose the number the computer is thinking.
+User will lose a point out of the ten for every incorrect guess.
+The Computer will let user know after each guess if the number the computer is thinking is higher or lower.
+Once user has chosen the correct number the computer will let user know.
+From here the computer will let user know how many guesses it took out of the 10 points to tell user the correct number the computer was thinking.
+The Computer will think of a new number and we'd repeat back from step one.
