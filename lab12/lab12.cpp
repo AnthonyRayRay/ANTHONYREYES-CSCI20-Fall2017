705888d77db46f72b918c4268b89d79e20bef5ed
Anthony Reyes <areyes011@student.butte.edu>
2017-08-30 01:37:56 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+cout << " ^----^ " <<endl ;
+cout << "(=- 3 -) Meowing~`" <<endl ;
+cout <<"(..)__(..)"<<endl ; 
+cout <<"[________]" <<endl ;
+cout <<" [______] " <<endl ;
+
+}
+
