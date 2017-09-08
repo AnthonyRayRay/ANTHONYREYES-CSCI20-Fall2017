e3d75df0477467ebc6edcdbcb94ce99818a76e71
Anthony Reyes <areyes011@student.butte.edu>
2017-09-08 05:22:06 +0000

    Lab1.5 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,106 @@
+ #include <iostream> 
+ #include <string>
+  using namespace std;
+ 
+  
+  int main () {
+      
+    string pluralNoun1 ;
+    string adjective1;
+    string adjective2;
+    string noun1;
+    string noun2;
+    string noun3;
+    string noun4;
+    string adjective3; 
+    string partOfTheBody1;
+    string pluralNoun2;
+    string adjective4;
+    string noun5;
+    string adjective5;
+    string noun6;
+    string partOfTheBody2;
+    string partOfTheBody3;
+    string pluralNoun3;
+    string adjective6;
+    string noun7;
+    string verb;
+    
+    cout << " Enter a Plural Noun: " ;
+    cin >> pluralNoun1; 
+    
+    cout << " Enter a Adjective: " ;
+    cin >> adjective1; 
+    
+    cout << " Enter a Adjective: " ;
+    cin >> adjective2;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun1;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun2;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun3;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun4;
+    
+    cout << " Enter a Adjective: " ;
+    cin >> adjective3;
+    
+    cout << " Enter a Part of the Body: " ; 
+    cin >> partOfTheBody1;
+    
+    
+    cout << " Enter a Plural Noun: " ;
+    cin >> pluralNoun2;
+    
+    cout << " Enter a Adjective: " ;
+    cin >> adjective4;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun5;
+    
+    cout << " Enter a Part of the Body: " ; 
+    cin >> partOfTheBody2;
+    
+    cout << " Enter a Adjective: " ;
+    cin >> adjective5;
+    
+    cout << " Enter a Noun: " ;
+    cin >> noun6;
+    
+    cout << " Enter a Part of the Body: " ;
+    cin >> partOfTheBody3;
+    
+    cout << " Enter a Plural Noun: ";
+    cin >> pluralNoun3;
+    
+    cout << " Enter a Adjective: ";
+    cin >> adjective6;
+    
+    cout << " Enter a Noun: ";
+    cin >> noun7;
+    
+    cout << " Enter a Verb: ";
+    cin >> verb;
+    
+    cout << endl;
+    
+    
+    
+    
+    cout <<  pluralNoun1 << " of all ages enjoy watching " << adjective1 << " magicians perform their " << adjective2 << " tricks." << endl ;
+    cout << "Every man, woman, and " << noun1 << " loves to see a magician pull a/an " << noun2 << " out of a hat, saw a live " << noun3 << " in half, or make a huge " << noun4 << " disappear into " << adjective3 << " air." << endl ;
+    cout << "Audiences love when magicians perform sleight of " << partOfTheBody1 << " with a deck of " << pluralNoun2 << ", a/an " << adjective4 << " coin, or a silk " << noun5 << "." << endl ; 
+    cout << "The greatest of all magicians was the " << adjective5 << " Harry Houdini, who was able to escape from a locked " << noun6 << " even though his " << partOfTheBody2 << " were tied behind his " << partOfTheBody3 << " and his feet were wrapped in iron " << pluralNoun3 << "." << endl;
+    cout << "Let us never forget the " << adjective6<< " " << noun7 << " who swallowed fire and " << verb << " no burns on them. "<< endl ;
+    
+    return 0;
+  }
+    
+    
+ 
+    
