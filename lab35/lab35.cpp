02d2b766b057b9aef907369bc7f7e0424fba1186
Anthony Reyes <areyes011@student.butte.edu>
2017-10-25 07:36:02 +0000

    lab35 submission

--- lab35/lab35.cpp
+++ lab35/lab35.cpp
@@ -15,7 +15,7 @@ class TemperatureConverter {
     void SetTempFromKelvin(float kelvin){kelvin_ = kelvin;}
     void SetTempFromCelsius(float celsius){
       if(celsius <= -273.15) {
-        cout << "\nEnter higher number then -273.15: "; // telling user to input a number above -273.15
+        cout << "\nEnter higher number then -273.15: "; // telling user to input a number above -273.1
         cin >> celsius;
       }
       kelvin_ = celsius + 273.15;
@@ -49,32 +49,7 @@ class TemperatureConverter {
   
 
   
- #include<iostream>
-#include "lab35.cpp"
-using namespace std;
 
-int main ()
-{
-    TemperatureConverter temp1; //testing default constructor
-    TemperatureConverter temp2(274); //testing overloaded constructor
-    
-    temp1.PrintTemperatures();
-    temp2.PrintTemperatures();
-    
-    temp1.SetTempFromKelvin(400.15); //testing mutator function
-    cout<<temp1.GetTempFromKelvin()<<endl;//testing accessor function
-    temp1.PrintTemperatures();
-    
-    temp2.SetTempFromCelsius(32); //testing other functions
-    cout<<temp2.GetTempAsCelsius()<<endl;
-    temp2.PrintTemperatures();
-    
-    temp2.SetTempFromFahrenheit(32);
-    cout<<temp2.GetTempAsFahrenheit()<<endl;
-    temp2.PrintTemperatures();
-    
-    return 0;
-}
   
   
   
